84f5487776b5581a8b4e10157e4950f9049cad12
manh19112004 <164605812+manh19112004@users.noreply.github.com>
2025-06-22 23:08:11 +0700

    Add files via upload

---
+++ gửi và nhận/EmailSenderReceiverApp/Form1.Designer.cs
@@ -0,0 +1,94 @@
+namespace EmailSenderReceiverApp
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.TextBox txtFrom;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtTo;
+        private System.Windows.Forms.TextBox txtSubject;
+        private System.Windows.Forms.TextBox txtBody;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Button btnReceive;
+        private System.Windows.Forms.ListBox lstInbox;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null)) components.Dispose();
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.txtFrom = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtTo = new System.Windows.Forms.TextBox();
+            this.txtSubject = new System.Windows.Forms.TextBox();
+            this.txtBody = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.btnReceive = new System.Windows.Forms.Button();
+            this.lstInbox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // txtFrom
+            this.txtFrom.Location = new System.Drawing.Point(12, 12);
+            this.txtFrom.Size = new System.Drawing.Size(360, 23);
+            this.txtFrom.PlaceholderText = "Email của bạn";
+            // 
+            // txtPassword
+            this.txtPassword.Location = new System.Drawing.Point(12, 41);
+            this.txtPassword.Size = new System.Drawing.Size(360, 23);
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.PlaceholderText = "Mật khẩu ứng dụng";
+            // 
+            // txtTo
+            this.txtTo.Location = new System.Drawing.Point(12, 70);
+            this.txtTo.Size = new System.Drawing.Size(360, 23);
+            this.txtTo.PlaceholderText = "Người nhận";
+            // 
+            // txtSubject
+            this.txtSubject.Location = new System.Drawing.Point(12, 99);
+            this.txtSubject.Size = new System.Drawing.Size(360, 23);
+            this.txtSubject.PlaceholderText = "Tiêu đề";
+            // 
+            // txtBody
+            this.txtBody.Location = new System.Drawing.Point(12, 128);
+            this.txtBody.Multiline = true;
+            this.txtBody.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtBody.Size = new System.Drawing.Size(360, 100);
+            this.txtBody.PlaceholderText = "Nội dung email...";
+            // 
+            // btnSend
+            this.btnSend.Location = new System.Drawing.Point(297, 234);
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.Text = "Gửi";
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // btnReceive
+            this.btnReceive.Location = new System.Drawing.Point(12, 263);
+            this.btnReceive.Size = new System.Drawing.Size(150, 23);
+            this.btnReceive.Text = "Nhận Email (IMAP)";
+            this.btnReceive.Click += new System.EventHandler(this.btnReceive_Click);
+            // 
+            // lstInbox
+            this.lstInbox.FormattingEnabled = true;
+            this.lstInbox.ItemHeight = 15;
+            this.lstInbox.Location = new System.Drawing.Point(12, 292);
+            this.lstInbox.Size = new System.Drawing.Size(360, 124);
+            // 
+            // Form1
+            this.ClientSize = new System.Drawing.Size(384, 431);
+            this.Controls.Add(this.lstInbox);
+            this.Controls.Add(this.btnReceive);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtBody);
+            this.Controls.Add(this.txtSubject);
+            this.Controls.Add(this.txtTo);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtFrom);
+            this.Text = "Gửi & Nhận Email";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+    }
+}
---
+++ gửi và nhận/EmailSenderReceiverApp/README.txt
@@ -0,0 +1,45 @@
+==============================
+EmailSenderReceiverApp - C# WinForms
+==============================
+
+📦 Chức năng:
+-------------
+- Gửi email sử dụng Gmail (SMTP)
+- Nhận email mới nhất bằng IMAP từ Gmail
+- Nhập tài khoản và mật khẩu ứng dụng trong form
+- Giao diện đơn giản dễ dùng
+
+🧩 Yêu cầu:
+----------
+- Visual Studio 2022 hoặc mới hơn
+- .NET 6 SDK (hoặc cao hơn)
+- Gói NuGet: MailKit
+
+🛠 Cài đặt & chạy:
+------------------
+1. Mở file 'EmailSenderReceiverApp.sln' bằng Visual Studio
+2. Cài đặt thư viện MailKit:
+   - Vào menu Tools > NuGet Package Manager > Package Manager Console
+   - Chạy lệnh:
+     Install-Package MailKit
+3. Bấm Start (F5) để chạy chương trình
+
+🔐 Gmail cần:
+--------------
+- Mật khẩu ứng dụng (App Password), KHÔNG dùng mật khẩu chính
+  ➤ https://myaccount.google.com/apppasswords
+
+📝 Sử dụng:
+-----------
+1. Nhập email & mật khẩu ứng dụng
+2. Nhập địa chỉ người nhận, tiêu đề và nội dung
+3. Bấm nút "Gửi" để gửi email
+4. Bấm nút "Nhận Email" để lấy email mới nhất (hiển thị tiêu đề trong danh sách)
+
+📩 Hỗ trợ mở rộng:
+------------------
+- Gửi file đính kèm
+- Xem nội dung email chi tiết
+- Giao diện đẹp hơn (theo phong cách Gmail)
+
+Liên hệ nếu bạn cần hỗ trợ mở rộng thêm.
