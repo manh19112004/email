15fe6b033b9ac491f4e7c8e8af02570577848e07
manh19112004 <164605812+manh19112004@users.noreply.github.com>
2025-06-28 22:51:50 +0700

    Add files via upload
    sửa lỗi

--- GmailStyleWinFormsApp/MainForm.Designer.cs
+++ GmailStyleWinFormsApp/MainForm.Designer.cs
@@ -13,14 +13,17 @@ namespace GmailStyleWinFormsApp
         private System.Windows.Forms.Button btnAttach;
         private System.Windows.Forms.ListBox lstInbox;
         private System.Windows.Forms.TextBox txtEmailDetail;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel;
 
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing && (components != null)) components.Dispose();
-            base.Dispose(disposing);
-        }
+        //protected override void Dispose(bool disposing)
+        //{
+            //if (disposing && (components != null)) components.Dispose();
+          //  base.Dispose(disposing);
+        //}
 
         private void InitializeComponent()
+        {
+            private void InitializeComponent()
         {
             this.txtFrom = new System.Windows.Forms.TextBox();
             this.txtPassword = new System.Windows.Forms.TextBox();
@@ -32,65 +35,93 @@ namespace GmailStyleWinFormsApp
             this.btnAttach = new System.Windows.Forms.Button();
             this.lstInbox = new System.Windows.Forms.ListBox();
             this.txtEmailDetail = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
             this.SuspendLayout();
 
-            this.txtFrom.Location = new System.Drawing.Point(12, 12);
-            this.txtFrom.Size = new System.Drawing.Size(360, 23);
-            this.txtFrom.ReadOnly = true;
+            // tableLayoutPanel
+            this.tableLayoutPanel.ColumnCount = 3;
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33F));
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 34F));
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33F));
+            this.tableLayoutPanel.RowCount = 9;
+            for (int i = 0; i < 9; i++)
+                this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
 
-            this.txtPassword.Location = new System.Drawing.Point(12, 41);
-            this.txtPassword.Size = new System.Drawing.Size(360, 23);
+            this.tableLayoutPanel.Dock = DockStyle.Fill;
+            this.tableLayoutPanel.Padding = new Padding(10);
+
+            // txtFrom
+            this.txtFrom.ReadOnly = true;
+            this.txtFrom.Dock = DockStyle.Fill;
+            this.tableLayoutPanel.Controls.Add(txtFrom, 0, 0);
+            this.tableLayoutPanel.SetColumnSpan(txtFrom, 3);
+
+            // txtPassword
             this.txtPassword.ReadOnly = true;
             this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Dock = DockStyle.Fill;
+            this.tableLayoutPanel.Controls.Add(txtPassword, 0, 1);
+            this.tableLayoutPanel.SetColumnSpan(txtPassword, 3);
 
-            this.txtTo.Location = new System.Drawing.Point(12, 70);
-            this.txtTo.Size = new System.Drawing.Size(360, 23);
+            // txtTo
+            this.txtTo.PlaceholderText = "Địa chỉ người nhận";
+            this.txtTo.Dock = DockStyle.Fill;
+            this.tableLayoutPanel.Controls.Add(txtTo, 0, 2);
+            this.tableLayoutPanel.SetColumnSpan(txtTo, 3);
 
-            this.txtSubject.Location = new System.Drawing.Point(12, 99);
-            this.txtSubject.Size = new System.Drawing.Size(360, 23);
+            // txtSubject
+            this.txtSubject.PlaceholderText = "Tiêu đề email";
+            this.txtSubject.Dock = DockStyle.Fill;
+            this.tableLayoutPanel.Controls.Add(txtSubject, 0, 3);
+            this.tableLayoutPanel.SetColumnSpan(txtSubject, 3);
 
-            this.txtBody.Location = new System.Drawing.Point(12, 128);
-            this.txtBody.Size = new System.Drawing.Size(360, 80);
+            // txtBody
             this.txtBody.Multiline = true;
+            this.txtBody.PlaceholderText = "Soạn nội dung email...";
+            this.txtBody.Dock = DockStyle.Fill;
+            this.tableLayoutPanel.Controls.Add(txtBody, 0, 4);
+            this.tableLayoutPanel.SetColumnSpan(txtBody, 3);
+            this.tableLayoutPanel.RowStyles[4].Height = 100;
 
-            this.btnAttach.Location = new System.Drawing.Point(12, 214);
-            this.btnAttach.Size = new System.Drawing.Size(120, 23);
-            this.btnAttach.Text = "Đính kèm";
+            // Buttons: Attach, Send, Receive
+            this.btnAttach.Text = "📎 Đính kèm";
+            this.btnSend.Text = "✉️ Gửi";
+            this.btnReceive.Text = "📥 Nhận Email";
+
+            this.btnAttach.Dock = DockStyle.Fill;
+            this.btnSend.Dock = DockStyle.Fill;
+            this.btnReceive.Dock = DockStyle.Fill;
+
             this.btnAttach.Click += new System.EventHandler(this.btnAttach_Click);
-
-            this.btnSend.Location = new System.Drawing.Point(138, 214);
-            this.btnSend.Size = new System.Drawing.Size(75, 23);
-            this.btnSend.Text = "Gửi";
             this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
-
-            this.btnReceive.Location = new System.Drawing.Point(219, 214);
-            this.btnReceive.Size = new System.Drawing.Size(153, 23);
-            this.btnReceive.Text = "Nhận Email (IMAP)";
             this.btnReceive.Click += new System.EventHandler(this.btnReceive_Click);
 
-            this.lstInbox.Location = new System.Drawing.Point(12, 243);
-            this.lstInbox.Size = new System.Drawing.Size(360, 100);
-            this.lstInbox.SelectedIndexChanged += new System.EventHandler(this.lstInbox_SelectedIndexChanged);
+            this.tableLayoutPanel.Controls.Add(btnAttach, 0, 5);
+            this.tableLayoutPanel.Controls.Add(btnSend, 1, 5);
+            this.tableLayoutPanel.Controls.Add(btnReceive, 2, 5);
 
-            this.txtEmailDetail.Location = new System.Drawing.Point(12, 349);
-            this.txtEmailDetail.Size = new System.Drawing.Size(360, 100);
+            // lstInbox
+            this.lstInbox.Dock = DockStyle.Fill;
+            this.lstInbox.SelectedIndexChanged += new System.EventHandler(this.lstInbox_SelectedIndexChanged);
+            this.tableLayoutPanel.Controls.Add(lstInbox, 0, 6);
+            this.tableLayoutPanel.SetColumnSpan(lstInbox, 3);
+            this.tableLayoutPanel.RowStyles[6].Height = 100;
+
+            // txtEmailDetail
             this.txtEmailDetail.Multiline = true;
             this.txtEmailDetail.ReadOnly = true;
+            this.txtEmailDetail.Dock = DockStyle.Fill;
+            this.tableLayoutPanel.Controls.Add(txtEmailDetail, 0, 7);
+            this.tableLayoutPanel.SetColumnSpan(txtEmailDetail, 3);
+            this.tableLayoutPanel.RowStyles[7].Height = 100;
 
-            this.ClientSize = new System.Drawing.Size(384, 461);
-            this.Controls.Add(this.txtFrom);
-            this.Controls.Add(this.txtPassword);
-            this.Controls.Add(this.txtTo);
-            this.Controls.Add(this.txtSubject);
-            this.Controls.Add(this.txtBody);
-            this.Controls.Add(this.btnAttach);
-            this.Controls.Add(this.btnSend);
-            this.Controls.Add(this.btnReceive);
-            this.Controls.Add(this.lstInbox);
-            this.Controls.Add(this.txtEmailDetail);
+            // Add panel to form
+            this.Controls.Add(this.tableLayoutPanel);
+
+            // Form
             this.Text = "Gửi & Nhận Email - Gmail Style";
+            this.ClientSize = new System.Drawing.Size(500, 600);
             this.ResumeLayout(false);
-            this.PerformLayout();
         }
     }
 }
